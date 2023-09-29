05305d080109ad78cfaf47ee59d20bcdf1dfbab1
Hedi-2P1 <mohamedhedi.kefi@esprit.tn>
2023-09-29 12:36:21 +0100

    For Jenkins

--- src/main/java/tn/esprit/skiproject/Entities/Test.java
+++ src/main/java/tn/esprit/skiproject/Entities/Test.java
@@ -2,7 +2,7 @@ package tn.esprit.skiproject.Entities;
 
 /* This is a test */
 public class Test {
-    private int numdzzzz;
+    private int nu;
     private String nom;
     private String test2;
 }
